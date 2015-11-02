58f9ff697fa12d21bb825eeee9f939f5bf5e3fed
arkal <aarao@ucsc.edu>
2015-11-01 21:33:30 -0800

    Added tempdir info to all java using tools

--- mutect_wrapper.sh
+++ mutect_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Call tool with parameters
-java $JAVA_OPTS -jar /opt/pimmuno/mutect/mutect.jar -T MuTect $@
+java $JAVA_OPTS -Djava.io.tmpdir=/data/tmp -jar /opt/pimmuno/mutect/mutect.jar -T MuTect $@
 
 # Fix ownership of output files
 UID=$(stat -c '%u:%g' /data)
--- picard_wrapper.sh
+++ picard_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Call tool with parameters
-java $JAVA_OPTS -jar /opt/pimmuno/picard/picard.jar $@
+java $JAVA_OPTS -Djava.io.tmpdir=/data/tmp -jar /opt/pimmuno/picard/picard.jar $@
 
 # Fix ownership of output files
 UID=$(stat -c '%u:%g' /data)
